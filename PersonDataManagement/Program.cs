44e84ec0f48180ba4b7cee23a06a64da7e64d5d5
Radhika <radhika.shankar1220@gmail.com>
2021-07-24 15:19:50 +0530

    [Radhika] searching specific name in list

--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -14,6 +14,7 @@ namespace PersonDataManagement
             Console.WriteLine("Enter 2. Retrieve top two records which age is less than 60");
             Console.WriteLine("Enter 3. Retrieve Records which is Age between 13 to 18");
             Console.WriteLine("Enter 4. Calculating average age");
+            Console.WriteLine("Enter 5.Search Specific name in list");
             Console.WriteLine("Enter the option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
@@ -22,16 +23,19 @@ namespace PersonDataManagement
                     PersonData.AddPerson(list);
                     break;
                 case 2:
-                    PersonData.AddPerson(list);
+                 
                     PersonData.RetrieveTopTwoLessThan60(list);
                     break;
                 case 3:
-                    PersonData.AddPerson(list);
+                  
                     PersonData.RetrieveTeenageRecords(list);
                     break;
                 case 4:
-                    PersonData.AddPerson(list);
+                   
                     PersonData.CalculateAverageAge(list);
+                    break;
+                case 5:
+                    PersonData.SearchingSpecificName(list,"Arun");
                     break;
                 default:
                     Console.WriteLine("Enter valid option");
