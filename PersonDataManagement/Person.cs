98582cfe52794c723918d352f6010e8dcd120175
Radhika <radhika.shankar1220@gmail.com>
2021-07-24 09:43:21 +0530

    [Radhika] adding the person details

---
+++ PersonDataManagement/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonDataManagement
+{
+   public class Person
+    {
+        //Getter and setter for fields
+        public int SSN { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public int age { get; set; }
+
+
+    }
+}
